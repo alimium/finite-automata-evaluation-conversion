f4e915b14c73e8abb09a8040908426995726365b
Ali Mehrabi <alimehrabi002@gmail.com>
2023-06-21 14:03:46 +0330

    Add string evaluation option to automatas

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from finite_automata.finite_automata import FiniteAutomata as FA
 # Create a finite automata
 fa = FA(states=5,
         alphabet=['a', 'b'],
-        trans=[(0, 'a', 1), (0, 'b', 2), (1, 'a', 3), (2, 'b', 3), (3, 'a', 4), (3, 'b', 0)],
+        trans=[(0, 'a', 1), (0, 'a', 2), (0, 'b', 2), (1, 'a', 3),
+               (2, 'b', 3), (3, 'a', 4), (3, 'b', 0), (3, 'b', 5)],
         init_state=0,
         accepting=[4])
 
 # Display the finite automata graphically
 fa.display()
+print(fa.evaluate('aabbbbaba', detailed=True))
