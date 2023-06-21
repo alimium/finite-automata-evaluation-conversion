14f8a2405b39ad0089016f0ef830438f11d82ca9
Ali Mehrabi <alimehrabi002@gmail.com>
2023-06-21 12:08:32 +0330

    Add sample driver code

---
+++ main.py
@@ -0,0 +1,11 @@
+from finite_automata.finite_automata import FiniteAutomata as FA
+
+# Create a finite automata
+fa = FA(states=5,
+        alphabet=['a', 'b'],
+        trans=[(0, 'a', 1), (0, 'b', 2), (1, 'a', 3), (2, 'b', 3), (3, 'a', 4), (3, 'b', 0)],
+        init_state=0,
+        accepting=[4])
+
+# Display the finite automata graphically
+fa.display()
